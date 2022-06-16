b7170cb701e649ef77193656871164cd8fe0ddd9
Lais Correa <laiscorreamartins@gmail.com>
2022-06-16 18:57:50 -0300

    criação da tabela sobrenomes e inserção

---
+++ tabelaSobrenomes.sql
@@ -0,0 +1,21 @@
+create table sobrenomes (
+	sobrenome varchar (90)
+);
+
+insert into sobrenomes values ('da Silva');
+insert into sobrenomes values ('Pereira');
+insert into sobrenomes values ('Machado');
+insert into sobrenomes values ('Souza');
+insert into sobrenomes values ('Santos');
+insert into sobrenomes values ('Sobral');
+insert into sobrenomes values ('Guedes');
+insert into sobrenomes values ('Marques');
+insert into sobrenomes values ('da Vila');
+insert into sobrenomes values ('Costa');
+insert into sobrenomes values ('Oliveira');
+insert into sobrenomes values ('Alves');
+insert into sobrenomes values ('Ferreira');
+insert into sobrenomes values ('Lima');
+insert into sobrenomes values ('Gomes');
+insert into sobrenomes values ('Ribeiro');
+insert into sobrenomes values ('Martins');
