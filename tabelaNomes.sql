9619fae0689cf1f35c0f685301088eacc7892bf2
Lais Correa <laiscorreamartins@gmail.com>
2022-06-16 18:57:26 -0300

    criacao da tabela de nomes e inserção

---
+++ tabelaNomes.sql
@@ -0,0 +1,21 @@
+create table nomes(
+	nome varchar (50)
+);
+
+insert into nomes values ('Fabiano');
+insert into nomes values ('Salvio');
+insert into nomes values ('Roberta');
+insert into nomes values ('Juliana');
+insert into nomes values ('Luciana');
+insert into nomes values ('Rochelle');
+insert into nomes values ('Alice');
+insert into nomes values ('Rodrigo');
+insert into nomes values ('Cristovao');
+insert into nomes values ('Mateus');
+insert into nomes values ('Roberto');
+insert into nomes values ('Lucas');
+insert into nomes values ('Ingrid');
+insert into nomes values ('Suzana');
+insert into nomes values ('Maria');
+insert into nomes values ('José');
+insert into nomes values ('João');
