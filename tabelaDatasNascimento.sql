73d12012e712c9be40954ecb893cafae0c4200ea
Lais Correa <laiscorreamartins@gmail.com>
2022-06-16 18:56:54 -0300

    criacao da tabela de datas e inserção

---
+++ tabelaDatasNascimento.sql
@@ -0,0 +1,20 @@
+create table datas_de_nascimento (
+	data_de_nascimento date
+)
+
+
+insert into datas_de_nascimento values ('15/07/1986');
+insert into datas_de_nascimento values ('31/05/1992');
+insert into datas_de_nascimento values ('22/07/1984');
+insert into datas_de_nascimento values ('08/10/1988');
+insert into datas_de_nascimento values ('08/09/1999');
+insert into datas_de_nascimento values ('29/10/1992');
+insert into datas_de_nascimento values ('17/02/1966');
+insert into datas_de_nascimento values ('04/05/1973');
+insert into datas_de_nascimento values ('26/03/1997');
+insert into datas_de_nascimento values ('14/03/1968');
+insert into datas_de_nascimento values ('02/01/1969');
+insert into datas_de_nascimento values ('11/06/1990');
+insert into datas_de_nascimento values ('01/05/1967');
+insert into datas_de_nascimento values ('27/03/1978');
+insert into datas_de_nascimento values ('04/06/1987');
