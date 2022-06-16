84bd70d541fe8e902886380933abf69477ab8484
Lais Correa <laiscorreamartins@gmail.com>
2022-06-16 18:56:20 -0300

    criação da tabela cidades e inserção dos nomes

---
+++ nomesCidades.sql
@@ -0,0 +1,20 @@
+create table cidades (
+	nome varchar (50)
+);
+
+insert into cidades values ('Alvorada');
+insert into cidades values ('Viamão');
+insert into cidades values ('Cidreira');
+insert into cidades values ('Pinhal');
+insert into cidades values ('Tramandai');
+insert into cidades values ('Imbe');
+insert into cidades values ('Guaiba');
+insert into cidades values ('Belo Horizonte');
+insert into cidades values ('Natal');
+insert into cidades values ('Rio de Janeiro');							   
+insert into cidades values ('São Paulo');							   
+insert into cidades values ('Gramado');							   
+insert into cidades values ('Canela');
+insert into cidades values ('Recife');
+insert into cidades values ('Taubaté');
+insert into cidades values ('Manaus');	
