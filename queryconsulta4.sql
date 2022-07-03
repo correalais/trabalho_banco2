8d2436ceeda27e1d356471bc3b4e10985c683489
Lais Correa <laiscorreamartins@gmail.com>
2022-07-03 15:12:05 -0300

    mudanças na query da consulta 4

--- queryconsulta4.sql
+++ queryconsulta4.sql
@@ -1,10 +1,34 @@
 SELECT 
 	cidade, COALESCE(SUM((datasaida - dataentrada) * valordiaria), 0) AS "Valor Investido"
+	
 FROM hospede, hospedagem
 WHERE
 	hospedagem.codhospede = hospede.codhospede
 AND 
 	COALESCE(datasaida - dataentrada, 0) <= 90
+	
+AND (SELECT * FROM calculaDiaria())
 
-GROUP BY cidade
+GROUP BY cidade, datasaida
 ORDER BY "Valor Investido" desc
+
+------------------------------------
+
+create or replace function calculaDiaria()  returns boolean as
+$$
+declare
+data_saida date;
+
+begin
+select datasaida into data_saida from hospede, hospedagem where hospedagem.codhospede = hospede.codhospede;
+if data_saida is not null then
+	perform (CURRENT_DATE - dataentrada)  * valordiaria from hospede, hospedagem where hospedagem.codhospede = hospede.codhospede;
+	return true;
+else
+	return false;
+end if;
+end;
+$$
+language plpgsql;
+
+select * from calculaDiaria()
